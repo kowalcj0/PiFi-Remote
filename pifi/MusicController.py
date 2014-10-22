018df84564d0c023346edf62834cab25248bdcbe
Bertrand Boichon <b.boichon@gmail.com>
2014-10-22 00:09:09 -0400

    Modified MusicController.py

--- pifi/MusicController.py
+++ pifi/MusicController.py
@@ -49,7 +49,7 @@ def computeRMS(fifoFile, sampleSize, scale):
         rawSamples = fifoFile.read(sampleSize) 
         if rawSamples and len(rawSamples) == sampleSize:
             rms = float(audioop.rms(rawSamples, 1))
-            level2 = min(rms/256.0, 1.0)
+            level2 = min(rms/256.0*100.0, 1.0)
             level3 = level2**exponent
             level = int(level3*scale)
             logging.info("Level= %f %d %d %d", rms, level, level2, level3)

453f9dc43d08cd310eab886904c903ca91f16ea5
Bertrand Boichon <b.boichon@gmail.com>
2014-10-23 00:24:01 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'MusicController = pifi.MusicController:main',
+            'musiccontroller = pifi.MusicController:main',
             'pifi-display = pifi.PiFiDisplay:main',
             'pifi-remote = pifi.PiFiRemote:main',
         ],

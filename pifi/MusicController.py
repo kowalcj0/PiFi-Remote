004404d6c16495f7c9227be5d40326c17a3a82f0
Bertrand Boichon <b.boichon@gmail.com>
2014-10-27 22:30:03 -0400

    Modified MusicController.py

--- pifi/MusicController.py
+++ pifi/MusicController.py
@@ -49,7 +49,7 @@ def computeRMS(fifoFile, sampleSize, scale):
             level1 = min(rms/256.0, 1.0)
             level2 = level1**exponent
             level = int(level2*scale*5*10**(exponent-3))
-            logging.info("Level= %f %f %f %f", rms, level1, level2, level)
+            #logging.info("Level= %f %f %f %f", rms, level1, level2, level)
     except Exception as e:
         logging.error("%s", e)    
     return level

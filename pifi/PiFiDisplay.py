1baa13e0198736445f3bf076c285fdce90edb5e9
Bertrand Boichon <b.boichon@gmail.com>
2014-10-28 01:14:48 -0400

    Modified PiFiDisplay.py

--- pifi/PiFiDisplay.py
+++ pifi/PiFiDisplay.py
@@ -21,7 +21,6 @@ logging.basicConfig(level=logging.INFO,
 def exitHandler(signal, frame):
     logging.info("Signaling internal jobs to stop...")
     try:
-        os.system("mpc toggle")
         os.system("mpc stop")
         mStop.set()
     except:

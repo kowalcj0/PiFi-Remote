b80b29583e84d928c80e772a78a74cbee0e9ea69
Bertrand Boichon <b.boichon@gmail.com>
2014-11-03 19:29:31 -0500

    Modified PiFiDisplay.py

--- pifi/PiFiDisplay.py
+++ pifi/PiFiDisplay.py
@@ -120,6 +120,7 @@ def refreshTrack(changeEvent, stopEvent):
                 if track is not None:
                     if prevTrack is None or track[0] != prevTrack[0]:
                         stopExternalStreaming()
+                        changeEvent.set()
                         LCD16x2.switchOn()
                         LCD16x2.setText(1, track[0], 0)
                     if prevTrack is None or track[1] != prevTrack[1]:

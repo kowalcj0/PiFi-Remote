bbcc859917f9bf70c95e3c115c7b96dc2e9040b3
Bertrand Boichon <b.boichon@gmail.com>
2014-11-26 00:04:13 -0500

    Modified setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.1.1',
+    version='1.1.2',
 
     description='Hi Fi music hub on Raspberry Pi',
     long_description=read('README.md'),
